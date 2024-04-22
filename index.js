5c41bdb8e2478e2b6b3fbebe8d1c3b7ae545c9a5
kseniya01 <155999972+kseniya01@users.noreply.github.com>
2024-04-22 12:43:07 +0000

    TOO MANY TO LIST LET US JUST GO AHEAD AND PUUUUUSH

--- index.js
+++ index.js
@@ -1,1 +1,7 @@
 // Add JavaScript below
+
+function myFunction() {
+    document.getElementById("demo").style.fontSize = "25px"; 
+    document.getElementById("demo").style.color = "red";
+    document.getElementById("demo").style.backgroundColor = "yellow";        
+  }
