9f8d236362f66ad79416514f946a3e260e851e6e
kseniya01 <155999972+kseniya01@users.noreply.github.com>
2024-04-23 11:18:01 +0000

    2

---
+++ postcss.config.js
@@ -0,0 +1,7 @@
+/* Add 'tailwindcss' and 'autoprefixer' to postcss.config.js file. */
+module.exports = {
+    plugins: {
+      tailwindcss: {},
+      autoprefixer: {},
+    }
+  }
--- tailwind.config.js
+++ tailwind.config.js
@@ -1,20 +1,8 @@
-/** CONFIG. TAILWIND
-Define customizations at the root of the project */
-@type {import('tailwindcss').Config}
-export default __CONFIG__
-
-/** CONFIG. TAILWIND TYPOGRAPHY
-Add Tailwind Typography plugin */
-@type {import('tailwindcss').Config}
+/* Add the paths to all template files. */
 module.exports = {
-  purge: [],
-  theme: {
-    extend: {},
-  },
-  variants: {
-
-  },
-  plugins: [
-    require('@tailwindcss/typography'),
-  ],
-}
+    content: ["./src/**/*.{html,js}"],
+    theme: {
+      extend: {},
+    },
+    plugins: [],
+  }
